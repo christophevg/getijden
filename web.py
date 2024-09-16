1e7e07ea6f117dc244d161f6e3eb2b73f2772ce2
Christophe VG <contact@christophe.vg>
2024-09-16 15:30:01 +0200

    added missing title

--- web.py
+++ web.py
@@ -17,7 +17,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def home():
-  return render_template("index.html")
+  return render_template("index.html", title="Getijden")
 
 @app.route("/api/cal/<location>")
 def cal(location):
